9030c03775f3e15bc5f0b71227f287b1f3f6a121
Suvayan Mondal <suvayan@sourcedeskglobal.com>
2024-06-03 13:19:34 +0530

    First commit using PC-2

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ console.log("Initial setup and commit");
 console.log(
   "I am using PC-1 and made this change now PC-2 has no way to know I've made this changes"
 );
+console.log("Using PC-2, I am working on a new branch named pc-2");
